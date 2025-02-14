7a653723b61381d5782309ac92da4a24b74d4c54
DiasRay <diassilvarayanne@gmail.com>
2025-02-14 12:44:33 -0300

    Heranças e Interface

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,6 +2,8 @@ package conta;
 
 import java.util.Scanner;
 import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
@@ -11,13 +13,22 @@ public class Menu {
 
 		Scanner print = new Scanner (System.in);
 		
-		// Teste da Classe Conta
-				Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-				c1.visualizar();
-				c1.sacar(12000.0f);
-				c1.visualizar();
-				c1.depositar(5000.0f);
-				c1.visualizar();
+		
+		 // Teste da Classe Conta Corrente
+			ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+			cc1.visualizar();
+			cc1.sacar(12000.0f);
+			cc1.visualizar();
+			cc1.depositar(5000.0f);
+			cc1.visualizar();
+			
+        // Teste da Classe Conta Poupança
+			ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+			cp1.visualizar();
+	        cp1.sacar(1000.0f);
+			cp1.visualizar();
+			cp1.depositar(5000.0f);
+			cp1.visualizar();
 		
 		int opcao;
 		
@@ -56,6 +67,7 @@ public class Menu {
 			switch (opcao) {
 				case 1:
 					System.out.println("Criar Conta\n\n");
+					
 					break;
 				case 2:
 					System.out.println("Listar todas as Contas\n\n");
